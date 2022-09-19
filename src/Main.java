88ef9a254d7642bf282179378db66e31317208c8
jelenavasili <jela.vasilijevic@gmail.com>
2022-09-19 23:52:11 +0200

    Added third print

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Neki ispis");
         System.out.println("Neki drugi ispis");
+        System.out.println("Neki treci ispis");
     }
 }
